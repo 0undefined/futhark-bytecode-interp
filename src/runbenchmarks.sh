985d5c6dacb8bf3d93383bcba12b22bae7a63542
0scar <example@example.org>
2023-01-08 18:06:37 +0100

    Improve benchmark scripts

--- src/runbenchmarks.sh
+++ src/runbenchmarks.sh
@@ -3,16 +3,29 @@
 BACKEND=opencl
 FILE=bench_simple.fut
 
-#IFS=$'\n' read -r -d '' -a SIMPLE_ENTRIES < <( sed -E '/^entry/!d;s/^entry ([^ ]+) .*/\1/g' "${FILE}" && printf '\0' )
-
 SIMPLE_ENTRIES=( $(sed -E '/^entry/!d;s/^entry ([^ ]+) .*/\1/g' "${FILE}") )
+BENCHMARK_DIR=${BENCHMARK_DIR:-.benchmarks}
 
 NUM_ENTRIES=$((${#SIMPLE_ENTRIES[@]}))
+LAST_PRINTLEN=$(( 0 ))
+
+! [ -d "${BENCHMARK_DIR}" ] && mkdir -p ${BENCHMARK_DIR}
+
 for i in "${!SIMPLE_ENTRIES[@]}"; do
   s=${SIMPLE_ENTRIES[$i]}
-  printf "[% ${#NUM_ENTRIES}i/$NUM_ENTRIES] $s              \r" "$((${i} + 1))"
-  ./sneaker -q --skip-csv --backend $BACKEND "${FILE}" -- -e $s
-  mv result-$BACKEND.bench result-${s}.bench
-done
 
-./sneaker --skip-benchmarks -r --backend $(echo -n "${SIMPLE_ENTRIES[*]}" | tr ' ' ',') "${FILE}"
+  # Clear the line by writing spaces
+  printf "%-${LAST_PRINTLEN}s\r" ""
+  STATUS=$(printf "[% ${#NUM_ENTRIES}i/$NUM_ENTRIES] $s\r" "$((${i} + 1))")
+  LAST_PRINTLEN=${#STATUS}
+  # Give the status counter
+  echo -n "$STATUS"
+
+  # Run the Benchmark
+  ./sneaker -q --skip-csv --backend $BACKEND "${FILE}" -- -e $s
+  # Save the benchmark
+  mv ${BENCHMARK_DIR}/result-$BACKEND.bench ${BENCHMARK_DIR}/result-${s}.bench
+done
+echo ""
+
+./sneaker --skip-benchmarks -r --backend $(echo -n "${SIMPLE_ENTRIES[*]}" | tr ' ' ',') "${FILE}" | tee benchmark.csv
